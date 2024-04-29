6bcec901b2b303a1dbeb396650d3a89b6118f0cb
DasonTio <d4sontiovino@gmail.com>
2024-04-29 09:26:16 +0700

    Initial Commit

---
+++ talktify/talktifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  talktifyApp.swift
+//  talktify
+//
+//  Created by Dason Tiovino on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct talktifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
